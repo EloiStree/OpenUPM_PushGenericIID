009ae51d35b4ce8bf70f934b74ec87619506df13
Éloi Strée <eloistree@gmail.com>
2024-04-23 00:45:58 +0200

    Save: 04/23/2024 12:45 AM

---
+++ Runtime/PushGenericMono_GamepadInspectorInput.cs
@@ -0,0 +1,117 @@
+﻿using GenericInputIID;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PushGenericMono_GamepadInspectorInput : MonoBehaviour
+{
+    public UnityEvent<int> m_onIntegerValueChanged;
+    public int m_startId = 2000000000;
+    public GamepadIntegerOnlyTwoAxisNoXR m_input;
+    public float m_joystickLeftHorizontal;
+    public float m_joystickLeftVertical;
+    public float m_joystickRightHorizontal;
+    public float m_joystickRightVertical;
+    public bool m_isFiring;
+
+    public int m_asIntCurrent;
+    public int m_asIntPrevious;
+
+    public float m_deathZone = 0.2f;
+
+
+    public float m_pushFrequence = 0.2f;
+
+
+    public int m_fireOnAction = 4561;
+    public int m_fireOffAction = 4560;
+
+
+
+    public void SetLeftJoystickVertical(float percent1to1)
+    {
+        m_joystickLeftVertical = percent1to1;
+    }
+    public void SetLeftJoystickHorizontal(float percent1to1)
+    {
+        m_joystickLeftHorizontal = percent1to1;
+    }
+    public void SetRightJoystickVertical(float percent1to1)
+    {
+        m_joystickRightVertical = percent1to1;
+    }
+    public void SetRightJoystickHorizontal(float percent1to1)
+    {
+        m_joystickRightHorizontal = percent1to1;
+    }
+
+
+    public void SetFireState(bool setOn) {
+        m_isFiring = setOn;
+    }
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+
+            m_onIntegerValueChanged.Invoke(m_asIntCurrent);
+            yield return new WaitForSeconds(m_pushFrequence);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+    private bool m_wasFiring;
+
+    void Update()
+    {
+        int value = m_startId;
+        value += (int)(TurnPercent11To099(m_joystickRightVertical));
+        value += (int)(TurnPercent11To099(m_joystickRightHorizontal) * 100);
+        value += (int)(TurnPercent11To099(m_joystickLeftVertical) * 10000);
+        value += (int)(TurnPercent11To099(m_joystickLeftHorizontal) * 1000000);
+
+        m_asIntCurrent = value;
+
+        
+        if(m_wasFiring!= m_isFiring)
+        {
+            m_wasFiring = m_isFiring;
+            if (m_isFiring)
+            {
+                m_onIntegerValueChanged.Invoke(m_fireOnAction);
+            }
+            else { 
+                m_onIntegerValueChanged.Invoke(m_fireOffAction);
+            }
+        }
+
+        if (m_asIntPrevious != m_asIntCurrent)
+        {
+            if (m_asIntCurrent == m_startId)
+            {
+                m_asIntPrevious = m_asIntCurrent;
+                m_onIntegerValueChanged.Invoke(m_asIntCurrent);
+            }
+            if (m_asIntPrevious == m_startId)
+            {
+                m_asIntPrevious = m_asIntCurrent;
+
+                m_onIntegerValueChanged.Invoke(m_asIntCurrent);
+            }
+
+        }
+
+    }
+
+    private float TurnPercent11To099(float percent)
+    {
+        if (percent == 0)
+            return 0;
+        if (Math.Abs(percent) < m_deathZone)
+            return 0;
+        return 1 + (((percent + 1f) * 0.5f) * 98f);
+    }
+}
